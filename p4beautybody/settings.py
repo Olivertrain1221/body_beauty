17f98e4c046f3010aa6ef78e5938311db3d68ba6
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-23 23:25:27 +0000

    Add settings to the gitpod workspace and env.py changes

--- p4beautybody/settings.py
+++ p4beautybody/settings.py
@@ -64,27 +64,22 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'p4beautybody.urls'
 
 
+
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
-    }
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
 
 WSGI_APPLICATION = 'p4beautybody.wsgi.application'
 
