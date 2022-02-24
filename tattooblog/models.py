404843fcace2e2bc08ff041753599c17bd23733d
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-24 16:47:27 +0000

    Add image to the models and also changed template to show

--- tattooblog/models.py
+++ tattooblog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class TattooPost(models.Model):
     title = models.CharField(max_length=20)
     slug = models.SlugField()
     date = models.DateField(auto_now_add=True)
-    authorofpost = models.TextField(blank=True, null=True, max_length=50)
-    # image =
-
+    body = models.TextField(blank=True, null=True, max_length=50)
+    image = CloudinaryField('image', default="https://res.cloudinary.com/dxsodecl1/image/upload/v1644248279/cld-sample.jpg")
+    #username added
 
 def __str__(self):
     return self.title
+
+
+def info(self):
+    return self.body[:20]
--- tattooblog/views.py
+++ tattooblog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from.models import TattooPost
 
 # Create your views here.
+
+
 def tattoo_gallery(request):
     """
     Runs the url.py tattoo_gallery
     """
-    tattooposts = TattooPost.objects.all()
-    return render(request, 'tattooblog/tattoo_gallery.html')
+    tattooposts = TattooPost.objects.all().order_by('date')
+    return render(request, 'tattooblog/tattoo_gallery.html', {'tattooposts': tattooposts})
