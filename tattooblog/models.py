e9909f01551e85ae10622af9eb49b1299eae8739
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-24 15:30:05 +0000

    Changed django version due to to many bugs operating 3.2

--- tattooblog/models.py
+++ tattooblog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class TattooPost(models.Model):
+    title = models.CharField(max_length=20)
+    slug = models.SlugField()
+    date = models.DateField(auto_now_add=True)
+    # image =
+
+
+def __str__(self):
+    return self.title
