8963ce03ef7e17e507c6600103af9e1ec4bb129b
Olivertrain1221 <olivetrain1996@gmail.com>
2022-03-30 15:12:15 +0100

    Add cripsy forms and styling for pages

--- tattooblog/forms.py
+++ tattooblog/forms.py
@@ -4,4 +4,4 @@ from . import models
 class CreatePost(forms.ModelForm):
     class Meta:
         model = models.TattooPost
-        fields = ['title', 'body', 'slug', 'image']
+        fields = ['title', 'body', 'image']
