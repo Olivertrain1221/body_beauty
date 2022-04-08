2f17d7de3d266375868a6c3b8cdccc0e8fd0ec1b
Olivertrain1221 <olivetrain1996@gmail.com>
2022-04-08 14:01:08 +0100

    Add update functionality to code

--- tattooblog/urls.py
+++ tattooblog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import TattooDetailListView, TattooListView, CreatePostView
-from . import views
+from .views import PostCreateView, TattooDetailListView, TattooListView, UpdatePostView
 
 
 app_name = 'tattooposts'
 
 urlpatterns = [
-    path('', TattooListView.as_view(), name="gallery"),
-    path('create/', CreatePostView.as_view(), name="create"),
+    path('', TattooListView.as_view(), name="tattoo_gallery"),
+    path('post/create/',PostCreateView.as_view(), name="tattoo_post_create"),
     path('<slug>', TattooDetailListView.as_view(), name="post_detail"),
+    path('<slug>/update/', UpdatePostView.as_view(), name="post_update"),
 ]
-
-
--- tattooblog/views.py
+++ tattooblog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import redirect, render
 from.models import TattooPost
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from . import forms
 from django.views.generic import (
     ListView,
     DetailView,
+    UpdateView,
     CreateView
-)
+    )
 
 # Create your views here.
 
@@ -25,27 +26,40 @@ class TattooDetailListView(DetailView):
     context_object_name = 'post'
 
 
-class CreatePostView(CreateView):
-    model = TattooPost
-    fields = ['titls', 'content']
-
 
 ## <app>/<model>_<viewtype>.html
 
 
-@login_required(login_url="/accounts/login/")
-def tattoo_create(request):
-    """
-    Allows the user to go to create a post
-    and saves to database once user has been confirmed
-    """
-    if request.method == 'POST':
-        form = forms.CreatePost(request.POST, request.FILES)
-        if form.is_valid():
-            case = form.save(commit=False)
-            case.author = request.user
-            case.save()
-            return redirect('tattooposts:gallery')
-    else:
-        form = forms.CreatePost()
-    return render(request, 'tattooblog/tattoo_post_create.html', {'form': form })
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = TattooPost
+    fields = ['title', 'body', 'image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# @login_required(login_url="/accounts/login/")
+# def tattoo_create(request):
+#     """
+#     Allows the user to go to create a post
+#     and saves to database once user has been confirmed
+#     """
+#     if request.method == 'POST':
+#         form = forms.CreatePost(request.POST, request.FILES)
+#         if form.is_valid():
+#             case = form.save(commit=False)
+#             case.author = request.user
+#             case.save()
+#             return redirect('tattooposts:gallery')
+#     else:
+#         form = forms.CreatePost()
+#     return render(request, 'tattooblog/tattoo_post_create.html', {'form': form })
+
+
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+    model = TattooPost
+    fields = ['title', 'body', 'image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
